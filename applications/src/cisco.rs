43df060325e265717b9c246d200d8262b650f1ea
IrynaBohdan <abogdan699@gmail.com>
2023-06-08 12:55:09 +0200

    feat:deviceIP & hostCount

--- applications/src/cisco.rs
+++ applications/src/cisco.rs
@@ -44,3 +44,32 @@ pub struct Device {
     pub typ: String,
     pub up_time: String,
 }
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct DeviceIp {
+    pub collection_status: String,
+    pub error_description: String,
+    pub global_credential_id: String,
+    pub id: String,
+    pub interface_count: String,
+    pub inventory_status_detail: String,
+    pub ip_addresses: Vec<String>,
+    pub last_update_time: String,
+    pub last_updated: String,
+    pub mac_address: String,
+    pub management_ip_address: String,
+    pub platform_id: String,
+    pub product_id: String,
+    pub reachability_failure_reason: String,
+    pub reachability_status: String,
+    pub serial_number: String,
+    #[serde(alias = "type")]
+    pub typ: String,
+    pub up_time: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct HostCount {
+    pub response: usize,
+    pub version: String,
+}
--- applications/src/main.rs
+++ applications/src/main.rs
@@ -2,10 +2,13 @@ use std::collections::HashMap;
 
 use anyhow::Result;
 use reqwest;
+use reqwest::header;
 use serde_json;
 use serde_json::Value;
 
 use crate::cisco::AllDevicesResponse;
+use crate::cisco::DeviceIp;
+use crate::cisco::HostCount;
 use crate::cisco::TicketResponse;
 mod cisco;
 
@@ -23,7 +26,7 @@ async fn main() -> Result<()> {
         .json()
         .await?;
     let json: TicketResponse = serde_json::from_value(res)?;
-    println!("ticket = {}", &json.response.service_ticket);
+    println!("ticket = {}\n", &json.response.service_ticket);
 
     let devices: Value = client
         .get("http://localhost:58000/api/v1/network-device")
@@ -34,7 +37,41 @@ async fn main() -> Result<()> {
         .await?;
     let vec_devices: AllDevicesResponse = serde_json::from_value(devices).unwrap();
 
-    println!("device = {:?}", &vec_devices.response[0]);
+    println!("device = {:?}\n", &vec_devices.response[0]);
+
+    let clients: Value = client
+        .get("http://localhost:58000/api/v1/network-device?hostType=Pc")
+        .header("X-Auth-Token", &json.response.service_ticket)
+        //.header("hostType", "Pc")
+        .send()
+        .await?
+        .json()
+        .await?;
+    let vec_clients: AllDevicesResponse = serde_json::from_value(clients).unwrap();
+
+    println!("client = {:?}\n", &vec_clients.response);
+
+    let deviceIp: Value = client
+        .get("http://localhost:58000/api/v1/network-device/ip-address/192.168.1.1")
+        .header("X-Auth-Token", &json.response.service_ticket)
+        .send()
+        .await?
+        .json()
+        .await?;
+    let vec_deviceIp: DeviceIp = serde_json::from_value(deviceIp).unwrap();
+
+    println!("deviceIp = {:?}\n", &vec_deviceIp);
+
+    let hostCount: Value = client
+        .get("http://localhost:58000/api/v1/host/count?hostType=Pc")
+        .header("X-Auth-Token", &json.response.service_ticket)
+        .send()
+        .await?
+        .json()
+        .await?;
+    let vec_host_count: HostCount = serde_json::from_value(hostCount).unwrap();
+
+    println!("hostCount = {:?}\n", &vec_host_count.response);
 
     Ok(())
 }
